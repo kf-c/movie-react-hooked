ebbcee9325895abffb7d1fe177c16bbca7e488c4
kf-c <hankfchan@gmail.com>
2019-10-27 13:30:14 +0800

    make components

--- src/App.js
+++ src/App.js
@@ -6,11 +6,52 @@ import Search from "./components/Search";
 
 const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=d08036ee";
 
-
 const App = () => {
+	const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+    useEffect(() => {
+    fetch(MOVIE_API_URL)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        setMovies(jsonResponse.Search);
+        setLoading(false);
+      });
+  }, []);
+
+	const search = searchValue => {
+    setLoading(true);
+    setErrorMessage(null);
+
+    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.Response === "True") {
+          setMovies(jsonResponse.Search);
+          setLoading(false);
+        } else {
+          setErrorMessage(jsonResponse.Error);
+          setLoading(false);
+        }
+      });
+  	};
   return (
     <div className="App">
-			<h1>Welcome to react</h1>
+		<Header text="React hooked" />
+      <Search search={search} />
+      <p className="App-intro">Sharing a few of our favourite movies</p>
+			<div className="movies">
+        {loading && !errorMessage ? (
+         <span>loading...</span>
+         ) : errorMessage ? (
+          <div className="errorMessage">{errorMessage}</div>
+        ) : (
+          movies.map((movie, index) => (
+            <Movie key={`${index}-${movie.Title}`} movie={movie} />
+          ))
+        )}
+      </div>
     </div>
   );
 }
